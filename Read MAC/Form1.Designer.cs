e8d092cdd80665d3e8a3ee7c6a8638540d6d33c0
Gábor <gaborfk@live.com>
2016-11-26 08:50:12 -0800

    Add project files.

---
+++ Read MAC/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Read_MAC
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBox = new System.Windows.Forms.RichTextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // txtBox
+            // 
+            this.txtBox.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtBox.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBox.Location = new System.Drawing.Point(13, 13);
+            this.txtBox.Name = "txtBox";
+            this.txtBox.Size = new System.Drawing.Size(823, 491);
+            this.txtBox.TabIndex = 0;
+            this.txtBox.Text = "";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(458, 574);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(314, 574);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 2;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 523);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(823, 23);
+            this.progressBar.TabIndex = 3;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(848, 627);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.txtBox);
+            this.Name = "frmMain";
+            this.Text = "Read MAC";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtBox;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.ProgressBar progressBar;
+    }
+}
+
---
+++ Read MAC/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Management;
+
+namespace Read_MAC
+{
+    public partial class frmMain : Form
+    {
+        List<string> lstPCs = new List<string>();
+
+        public frmMain()
+        {
+            string strInputFile="//cnsccscfs002/itadmin/Win2k/Remote Agents/Aruba orders/Input.txt";
+    
+            InitializeComponent();
+            lstPCs = Utilities.ReadAllText(strInputFile);
+           
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            int intProgressCount = 0;
+            
+            btnGo.Enabled = false;
+
+            progressBar.Minimum = 0;
+            progressBar.Maximum = lstPCs.Count;
+
+
+            txtBox.Clear();
+            
+            foreach (string strPC in lstPCs)
+            {
+    
+                string strProgressCount="";
+
+                intProgressCount += 1;
+
+                if (intProgressCount<10)
+                    strProgressCount = "000" + intProgressCount + " ";
+                else
+                    if (intProgressCount < 100)
+                        strProgressCount = "00" + intProgressCount + " ";
+                    else
+                        if (intProgressCount < 1000)
+                            strProgressCount = "0" + intProgressCount + " ";
+
+                txtBox.AppendText(strProgressCount + strPC + " ", Color.Black);
+
+                if (Utilities.isPCOnline(strPC))
+                {
+                    txtBox.AppendText("Online", Color.Green);
+                }
+                else
+                {
+                    txtBox.AppendText("Offline", Color.Red);
+                }
+               
+                txtBox.AppendText(Environment.NewLine);
+
+                progressBar.Value=intProgressCount;
+            }
+
+            txtBox.AppendText(Environment.NewLine);
+            txtBox.AppendText("Complete", Color.Blue);
+            btnGo.Enabled = true;
+        }
+    }
+
+    public static class RichTextBoxExtensions
+    {
+        public static void AppendText(this RichTextBox box, string text, Color color)
+        {
+            box.SelectionStart = box.TextLength;
+            box.SelectionLength = 0;
+            box.SelectionColor = color;
+            box.AppendText(text);
+            box.SelectionColor = box.ForeColor;
+        }
+    }
+
+    public static class Utilities
+    {
+        public static bool isPCOnline(string strPC)
+        {
+            ManagementScope scope = new ManagementScope(string.Format(@"\\{0}\root\cimv2", strPC));
+            ManagementClass os = new ManagementClass(scope, new ManagementPath("Win32_OperatingSystem"), null);
+            try
+            {
+                ManagementObjectCollection instances = os.GetInstances();
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+            
+        }
+
+        public static List<string> ReadAllText(string strFile)
+        {
+            string line;
+            List<string> lstData = new List<string>();
+          
+            using (StreamReader streamReader = new StreamReader(strFile, Encoding.UTF8))
+            {
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    lstData.Add(line);
+                }
+            }
+            return lstData;
+        }
+        
+    }
+}
